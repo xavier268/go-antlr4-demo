7191f50134b66ccca3e7f1c527f2cafca2258395
xavier <xavier@fed29onDellXPS>
2019-04-11 13:42:41 +0200

    working demo

--- main.go
+++ main.go
@@ -12,27 +12,60 @@ import (
 	"github.com/xavier268/go-antlr4-demo/internal/pkg/myparser"
 )
 
+var p *myparser.ExprParser
+
 func main() {
 
 	fmt.Println("Launching ...")
 
 	// Setup the input
-	is := antlr.NewInputStream("1 + 2 * 3")
+	const intxt = "1 + 2 * 3\ntiti = 56 + 68\n"
+	is := antlr.NewInputStream(intxt)
+	fmt.Println("Preparing to process : \n", intxt)
 
 	// Create the Lexer
 	lexer := myparser.NewExprLexer(is)
 
 	// Read all tokens
-	for {
-		t := lexer.NextToken()
-		if t.GetTokenType() == antlr.TokenEOF {
-			break
-		}
-		fmt.Printf("%d \t%s \t%q \n",
-			t.GetTokenType(),
-			lexer.SymbolicNames[t.GetTokenType()],
-			t.GetText())
-	}
+	// for {
+	// 	t := lexer.NextToken()
+	// 	if t.GetTokenType() == antlr.TokenEOF {
+	// 		break
+	// 	}
+	// 	fmt.Printf("%d \t%s \t%q \n",
+	// 		t.GetTokenType(),
+	// 		lexer.SymbolicNames[t.GetTokenType()],
+	// 		t.GetText())
+	// }
+
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the Parser
+	p = myparser.NewExprParser(stream)
+
+	// Build ast, starting with the "prog" rule
+	ast := p.Prog()
+	fmt.Println("Dumping ast : ", ast.ToStringTree(nil, p))
+
+	// Walk the tree with the dump listener
+	listener := new(dumpListener)
+	antlr.ParseTreeWalkerDefault.Walk(listener, ast)
+
 	return
 
+}
+
+//dumpListener will dump the rules and context
+type dumpListener struct {
+	*myparser.BaseExprListener
+}
+
+func (dl *dumpListener) ExitEveryRule(ctx antlr.ParserRuleContext) {
+	fmt.Printf("\n<<<< EXITING rule, context : %q, rule : %d\n", ctx.GetText(), ctx.GetRuleIndex())
+	fmt.Println(ctx.ToStringTree(nil, p))
+}
+
+func (dl *dumpListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+	fmt.Printf("\n>>>> ENTERING rule, context : %q, rule : %d\n", ctx.GetText(), ctx.GetRuleIndex())
+	fmt.Println(ctx.ToStringTree(nil, p))
 }
